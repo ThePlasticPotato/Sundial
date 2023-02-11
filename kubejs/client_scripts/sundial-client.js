190e9d33eb169ede0837ac0c59a4f584c52a5d69
FoxyFox909 <110856509+FoxyFox909@users.noreply.github.com>
2023-02-11 15:59:58 -0500

    KubeJS Client script
    hides EFLN

---
+++ kubejs/client_scripts/sundial-client.js
@@ -0,0 +1,10 @@
+// priority: 0
+
+console.info('Client script loaded')
+
+onEvent('jei.hide.items', event => {
+	// Hide items in JEI here
+	// event.hide('minecraft:cobblestone')
+	event.hide('tconstruct:efln_ball')
+	
+})
